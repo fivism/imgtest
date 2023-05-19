378f5e3e175466144c0807cb9863741723a5e5f7
Michael Fivis <michael@dagens.farm>
2023-05-20 01:29:42 +0200

    back: add a jimp flow for gaussian blur

--- back/app.ts
+++ back/app.ts
@@ -1,19 +1,28 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import jimp from "jimp";
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
-app.get('/', (req, res) => {
-	res.send('This is a test page we don\'t serve things here!');
-})
+app.get("/", (req, res) => {
+  res.send("This is a test page we don't serve things here!");
+});
 
-app.post('/jimp', (req, res) => {
-	res.send('Received!');
-})
+app.get("/jimp", (req, res) => {
+  res.send({ msg: "Nothing to get -- yet." });
+});
 
-app.get('/jimp', (req, res) => {
-	res.send({msg: 'Nothing to get -- yet.'});
-})
+app.post("/jimp/gaussian", async (req, res) => {
+  const { image } = req.body;
+  const decodedImage = await jimp.read(
+    Buffer.from(image.split(",")[1], "base64")
+  );
+  decodedImage.blur(5);
+  res.send(await decodedImage.getBase64Async(jimp.AUTO));
+});
 
 export default app;
