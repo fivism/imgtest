32b9b57f12a60fac4f13c8049e5fa7c131a3c8c5
Michael Fivis <michael@dagens.farm>
2023-05-20 01:59:22 +0200

    back: full regression test on the blur endpoint

--- back/app.test.ts
+++ back/app.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "./app";
+import { STOPLIGHT, STOPLIGHT_BLURRED } from "./examples/testfiles";
 
 describe("behavior on /jimp", () => {
   describe("hello world", () => {
@@ -12,11 +13,11 @@ describe("behavior on /jimp", () => {
   });
   describe("a gaussian blur is returned as expected", () => {
     test("should respond with a 200 status code and a message", async () => {
-      const response = await request(app).post("/jimp").send({
-        image: "username",
-        operation: "gaussianblur",
+      const response = await request(app).post("/jimp/gaussian").send({
+        image: STOPLIGHT,
       });
       expect(response.statusCode).toBe(200);
+      expect(response.text).toBe(STOPLIGHT_BLURRED);
     });
   });
 });
